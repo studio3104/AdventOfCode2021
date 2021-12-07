efdf3aa3b6b0f0b7f1afee8f3e4ce1c18128d3a5
studio3104 <studio3104.com@gmail.com>
2021-12-07 12:15:45 +0900

    Init day4

---
+++ src/main/java/com/studio3104/adventofcode2021/day04/Input.java
@@ -0,0 +1,33 @@
+package com.studio3104.adventofcode2021.day04;
+
+import com.studio3104.adventofcode2021.utilities.InputLoader;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Input {
+    int[] numbers;
+    List<int[][]> boards;
+
+    Input() {
+        String[] input = InputLoader.loadStringInput(4);
+
+        numbers = Arrays.stream(input[0].split(",")).mapToInt(Integer::parseInt).toArray();
+
+        boards = new ArrayList<>();
+        int[][] board = new int[5][5];
+        int bi = 0;
+        for (int i = 1; i < input.length; ++i) {
+            String line = input[i];
+            if (line.isBlank()) {
+                boards.add(board);
+                board = new int[5][5];
+                bi = 0;
+                continue;
+            }
+            board[bi++] = Arrays.stream(line.trim().split(" +")).mapToInt(Integer::parseInt).toArray();
+        }
+        boards.add(board);
+    }
+}
