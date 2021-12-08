fd1c60ba500e73b649ebaf7d0b2853539ccc1a2e
studio3104 <studio3104.com@gmail.com>
2021-12-09 03:12:49 +0900

    Answer part 1 of day 8

---
+++ src/main/java/com/studio3104/adventofcode2021/day08/Problem1.java
@@ -0,0 +1,21 @@
+package com.studio3104.adventofcode2021.day08;
+
+import com.studio3104.adventofcode2021.utilities.InputLoader;
+
+import java.util.Arrays;
+
+public class Problem1 {
+    static int getResult(String[] input) {
+        int count = 0;
+        for (String line : input) {
+            count += Arrays.stream(line.split(" \\| ")[1].trim().split(" +"))
+                    .filter(o -> o.length() >= 2 && o.length() <= 4 || o.length() == 7)
+                    .count();
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getResult(InputLoader.loadStringInput(8)));
+    }
+}
